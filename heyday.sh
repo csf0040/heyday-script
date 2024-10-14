749518ca21c707197bd7fbbda7fa8dc83bd93eb4
csf0040 <csf0040@auburn.edu>
2024-10-14 12:29:00 -0500

    Create heyday.sh

---
+++ heyday.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#user will be asked to type name
+echo "Hey! Please enter your name"
+
+#read will assign user input to the variable name
+read name 
+
+#print the Hey Day statement 
+echo "Hey, $name!"
+echo "Happy Hey Day and War Eagle!"
+
+
+
+ 
+
